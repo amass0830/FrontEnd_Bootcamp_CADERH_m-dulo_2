efa417aefe9cdb618a5800982c37e20c1a6c67da
Andrea Mass <amass0830@gmail.com>
2023-03-15 17:08:47 -0600

    Finish exercise 1, sesion 10

--- sesion_10/ejercicio1_modulo2/assets/js/logic.js
+++ sesion_10/ejercicio1_modulo2/assets/js/logic.js
@@ -1,10 +1,38 @@
-$(document).ready(function () {         
-        $("#text1").mouseout(function () {
-            $("#text2").hide();
+$(document).ready(
+    function () {         
+        $("#text1").mouseout(
+            function () {
+             $("#text2").hide();
         });
  
-    $("#text1").mouseenter(function () {
-        $("#text2").show();
+        $("#text1").mouseenter(
+            function () {
+             $("#text2").show();
+        });
+});
+
+$(document).ready(
+    function () {
+        $("#imgPacman").click(
+            function () {
+                $("#imgPacman").css("width", "100%");
+    });
+
+    $("#imgPacman").mouseout(
+        function () {
+            $("#imgPacman").css("width", "20%");
     });
 });
 
+$(document).ready(
+    function () {
+        $("#text3").dblclick(
+            function () {
+                $("#text3").css("font-size", "xx-large");
+    });
+
+    $("#text3").mouseout(
+        function () {
+            $("#text3").css("font-size", "medium");
+    });
+});
